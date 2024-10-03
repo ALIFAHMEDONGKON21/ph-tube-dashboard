43fa5e2b69f62bdec2d472ad90f0c1184c9a86ae
MD ALIF <mdalifahmed114510@gmail.com>
2024-10-04 03:57:11 +0600

    btn click show that type of song moves

--- js/index.js
+++ js/index.js
@@ -1,30 +1,78 @@
 const loadCategories=()=>{
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then(res =>res.json())
-    .then(data=>displayCategories(data.categories
-    ))
-    .catch(console.error('NOT FOUND'));
+    .then(data=>displayCategories(data.categories))
+    .catch((error) => console.log(error));
     
+}
+
+
+const loadCategoryVideos=(id)=>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(res =>res.json())
+    .then(data=>displayVideos(data.category))
+    .catch((error) => console.log(error));
 }
 
 
 const displayCategories=(categories)=>{
 
     const categori_container=document.getElementById('categories')
-    console.log(categories);
+    
 
     categories.forEach(item => {
-        console.log(item)
-        const button=document.createElement('button');
-        button.classList='btn';
-        button.innerText=item.category;
+    console.log(item)
+       
+        const button_container=document.createElement('div');
+        button_container.innerHTML=`
+        <button onclick=loadCategoryVideos(${item.category_id}) class="btn">
+        ${item.category}
+        </button>
+        `
         
-        categori_container.append(button)
+        categori_container.append(button_container)
 
 
     });
+}
 
+const loadVideos=()=>{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res =>res.json())
+    .then(data=>displayVideos(data.videos))
+    .catch((error) => console.log(error));
 
 }
 
+const displayVideos=(video)=>{
+    console.log(video)
+    const videos_container=document.getElementById('Videos');
+    videos_container.innerHTML="";
+
+    video.forEach(item =>{
+        console.log(item)
+
+        const card=document.createElement('div')
+        card.classList="card card-compact"
+        card.innerHTML=`
+        <figure>
+    <img
+      src="${item.thumbnail}"
+      alt="Shoes" />
+      </figure>
+     <div class="card-body">
+     <h2 class="card-title">Shoes!</h2>
+     <p>If a dog chews shoes whose shoes does he choose?</p>
+     <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+     </div>
+  </div>
+        `
+    videos_container.append(card)
+
+    })
+}
+
+
+loadVideos();
 loadCategories()
